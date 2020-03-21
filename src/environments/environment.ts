db3c474cfb951ee2c517f38d136193c5ec5348ab
IsaacUP <183411@ids.upchiapas.edu.mx>
2020-03-21 17:17:19 -0600

    first commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  wsUrl: 'http://35.174.104.40:3000/'
 };
 
 /*
